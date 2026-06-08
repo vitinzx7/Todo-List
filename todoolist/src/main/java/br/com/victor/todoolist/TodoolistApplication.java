94886f432a5c08c77db4f6f0bd4b4b410b510aee
vitinzx7 <victordideus@gmail.com>
2026-06-08 17:15:46 -0300

    Adicionando pastas de Camadas

---
+++ todoolist/src/main/java/br/com/victor/todoolist/TodoolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.victor.todoolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoolistApplication.class, args);
+	}
+
+}
---
+++ todoolist/src/test/java/br/com/victor/todoolist/TodoolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victor.todoolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
