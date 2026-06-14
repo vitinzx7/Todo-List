ac4c125da366e5729e25f26fa17d33076e112d64
vitinzx7 <victordideus@gmail.com>
2026-06-14 13:43:57 -0300

    colocando operadores e alguns imports  no Controller

--- todoolist/src/main/java/br/com/victor/todoolist/controller/TodoController.java
+++ todoolist/src/main/java/br/com/victor/todoolist/controller/TodoController.java
@@ -1,8 +1,11 @@
 package br.com.victor.todoolist.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.victor.todoolist.entity.Todo;
 import br.com.victor.todoolist.service.TodoService;
 
 @RestController
@@ -10,5 +13,23 @@ import br.com.victor.todoolist.service.TodoService;
 public class TodoController {
     private TodoService todoService;
 
-  
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+    
+    List<Todo> create(Todo todo) {
+        return todoService.create(todo);
+    }
+
+    List<Todo> list() {
+        return todoService.list();
+    }
+
+    List<Todo> update(Todo todo) {
+        return todoService.update(todo);
+    }
+
+    List<Todo> delete( Long id) {
+        return todoService.delete(id);
+    }
 }
